6039853227f97d31774dd5537a58942de4e55c39
Jackson Gariety <personal@jacksongariety.com>
2014-01-07 13:46:06 -0800

    Adds simple Emacs24 theme. To work properly, you must have Toy Chest installed in your terminal as well.

---
+++ themes/Emacs/toychesttheme.el
@@ -0,0 +1,13 @@
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(font-lock-comment-face ((t (:foreground "black" :weight semi-bold))))
+ '(font-lock-doc-face ((t (:inherit font-lock-string-face :foreground "black" :weight ultra-bold))))
+ '(font-lock-function-name-face ((t (:foreground "yellow" :weight bold))))
+ '(font-lock-keyword-face ((t (:foreground "red" :weight bold))))
+ '(font-lock-string-face ((t (:foreground "cyan" :weight normal))))
+ '(font-lock-variable-name-face ((t (:foreground "magenta" :weight light))))
+ '(js3-jsdoc-tag-face ((t (:foreground "yellow"))))
+ '(minibuffer-prompt ((t (:foreground "medium blue")))))
